30aae7c2fbb85c38adfe63bad1862433264e91c6
vojtech.cerny <ProrokCZ.PC@gmail.com>
2022-02-18 12:02:39 +0100

    něco

--- main.js
+++
@@ -1,6 +0,0 @@
-const movement
-const score
-const timer = getElementById()
-const enemy
-const sound
-const live
---
+++ res/main.js
@@ -0,0 +1,17 @@
+
+const score = document.getElementById("score");
+const timer = document.getElementById("timer");
+const player = document.getElementById("player");
+const live = document.getElementById("live");
+const canvas = document.getElementById("canvas");
+
+//HUD
+
+
+//canvas
+
+//movement
+
+//enemy
+
+//side scroll

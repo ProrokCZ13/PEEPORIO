c256269205b62084035de99a6ecaf1cce99bb190
vojtu <ProrokCZ.PC@gmail.com>
2022-04-21 13:51:51 +0100

    Hráč + gravitace

--- res/main.js
+++ res/main.js
@@ -1,17 +1,55 @@
-
-const score = document.getElementById("score");
-const timer = document.getElementById("timer");
-const player = document.getElementById("player");
-const live = document.getElementById("live");
 const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-//HUD
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let gravity = 0.5
+
+//const playerImg = new Image();
+//playerImg.src = "./media/img/PPR.png"
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x:0,
+            y:1
+        }
+        this.width = 10
+        this.height = 10
+    }
+
+    draw(){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x,
+                     this.position.y,
+                     this.height,
+                     this.width)
+    }
+    update(){
+        this.position.y += this.velocity.y
+        this.draw()
+
+        if (this.position.y +this.height +
+            this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
+}
 
 
-//canvas
+const player = new Player()
 
-//movement
 
-//enemy
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    player.update()
+}
 
-//side scroll
+animate()

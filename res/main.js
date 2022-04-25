c0d275d1a837fd5e2f47e4e55860e992a20104ff
vojtu <ProrokCZ.PC@gmail.com>
2022-04-25 08:45:56 +0200

    Ovládání

--- res/main.js
+++ res/main.js
@@ -5,10 +5,19 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-let gravity = 0.5
+let gravity = 0.5;
+let key = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+};
 
-//const playerImg = new Image();
-//playerImg.src = "./media/img/PPR.png"
+//hráč
+const playerImg = new Image();
+playerImg.src = "./media/img/PPR.png"
 
 class Player {
     constructor() {
@@ -33,6 +42,7 @@ class Player {
     }
     update(){
         this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
         this.draw()
 
         if (this.position.y +this.height +
@@ -50,6 +60,60 @@ function animate() {
     requestAnimationFrame(animate)
     ctx.clearRect(0,0, canvas.width, canvas.height)
     player.update()
+
+    if (key.right.pressed){
+        player.velocity.x = 5
+    }else if (key.left.pressed){
+        player.velocity.x = -5
+    }else player.velocity.x = 0
 }
 
 animate()
+//pohyb
+window.addEventListener('keydown', ({keyCode}) => {
+    switch (keyCode) {
+        //W
+        case 87:
+            player.velocity.y -= 20
+            break
+
+        //A
+        case 65||37:
+            key.left.pressed = true
+            break
+        //S
+        case 83||40:
+
+            break
+        //D
+        case 68||39:
+            key.right.pressed = true
+            break
+
+    }
+
+    
+})
+
+window.addEventListener('keyup', ({keyCode}) => {
+    switch (keyCode) {
+        //W
+        case 87:
+            player.velocity.y += 7
+            break
+
+        //A
+        case 65||37:
+            key.left.pressed = false
+            break
+        //S
+        case 83||40:
+
+            break
+        //D
+        case 68||39:
+            key.right.pressed = false
+            break
+
+    }
+})
